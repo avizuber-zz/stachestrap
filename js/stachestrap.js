936e1a1143b038dedf735a0e2c4b17feb2f77be6
avizuber <avi.zuber@gmail.com>
2014-10-28 21:26:48 -0400

    Updated styles for select items and file upload buttons. Began styling/conversions for radio buttons

--- js/stachestrap.js
+++ js/stachestrap.js
@@ -8,8 +8,34 @@ if (typeof jQuery === 'undefined') { throw new Error('Stachestrap\'s JavaScript 
 * Stachestrap: natural-language.js v1.0.0
 * ======================================================================== */
 var nlForm = $('#natural-language');
+
+/* Style File Upload Buttons in NL Form */
+var wrapper = $('<div/>').css({height:0,width:0,'overflow':'hidden'});
+var fileInput = $(':file').wrap(wrapper);
+
+fileInput.change(function(){
+    $this = $(this);
+    $('#nl-fileUpload').text($this.val());
+})
+
+$('#nl-fileUpload').click(function(){
+    fileInput.click();
+}).show();
+
+/* Convert Radio Buttons to Select Options, still needs major work*/
+var $checkbox = $('#natural-language .radio label input[type=radio]');
+var $select = $('<select></select>');    // create a select
+$select.attr('name', $checkbox.attr('name'));    // set name and value
+
+$checkbox.each(function(i, checkbox){
+    var str = $checkbox.eq(i).val();
+    $select.append($('<option>').val(str).text(str));
+});
+$checkbox.replaceWith($select);
+
+/* Help Determine Width of Inputs based on Placeholder text */
 var nlInputs = $('#natural-language input');
 
 for(i=0; i<nlInputs.length; i++){
-    nlInputs[i].setAttribute('size',nlInputs[i].getAttribute('placeholder').length);
+    nlInputs[i].setAttribute('size',nlInputs[i].getAttribute('placeholder').length-3);
 }
